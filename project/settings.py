5402003793927db34449ff36cab5100d890f1d5e
Caoilte Guiry <caoilte.guiry@gmail.com>
2012-01-01 18:24:19 +0000

    Adding a default entry for STATICFILES_DIRS (the 'static' folder above PROJECT_ROOT).

--- project/settings.py
+++ project/settings.py
@@ -54,6 +54,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, "static"),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
